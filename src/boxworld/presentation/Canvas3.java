fa62fb6632ba7e155ce085bd9f886832c68bce0a
Unknown <odarrietam@est.utn.ac.cr>
2017-08-24 19:50:29 -0600

    up

--- src/boxworld/presentation/Canvas3.java
+++ src/boxworld/presentation/Canvas3.java
@@ -16,21 +16,16 @@ import javax.swing.JPanel;
  */
 public class Canvas3 extends JPanel implements Runnable{
     
-    private final int C_WIDTH = 768; //ancho del canvas en pixeles
-    private final int C_HEIGHT = 768; //alto del canvas en pixeles
-    private final int SPRITE_SIZE = 48; //ancho y alto del sprite en pixeles
+    private final int C_WIDTH = 688; //ancho del canvas en pixeles
+    private final int C_HEIGHT = 688; //alto del canvas en pixeles
+    private final int SPRITE_SIZE = 43; //ancho y alto del sprite en pixeles
     
     private final Sprite[][] world = new Sprite[16][16];
-    
-    private final BufferedImage bufferedImage;
-    private Image skin1;
     
     public Canvas3() {
         setFocusable(true);
         setSize(new Dimension(C_WIDTH, C_HEIGHT));
         setPreferredSize(new Dimension(C_WIDTH, C_HEIGHT));
-        
-        this.bufferedImage = new BufferedImage(this.C_WIDTH, this.C_HEIGHT, BufferedImage.TYPE_INT_RGB);
         
         for(int i = 0; i < 16; i++){
             for(int j = 0; j < 16; j++){
@@ -45,56 +40,56 @@ public class Canvas3 extends JPanel implements Runnable{
         world[3][8] = new Sprite(Assets.WALL, 3, 8);
         world[3][9] = new Sprite(Assets.WALL, 3, 9);
         //Columna X=4
-        world[4][4] = new Sprite(Assets.WALL, 3, 9);
-        world[4][5] = new Sprite(Assets.WALL, 3, 9);
-        world[4][6] = new Sprite(Assets.WALL, 3, 9);
-        world[4][9] = new Sprite(Assets.WALL, 3, 9);
-        world[4][10] = new Sprite(Assets.WALL, 3, 9);
+        world[4][4] = new Sprite(Assets.WALL, 4, 4);
+        world[4][5] = new Sprite(Assets.WALL, 4, 5);
+        world[4][6] = new Sprite(Assets.WALL, 4, 6);
+        world[4][9] = new Sprite(Assets.WALL, 4, 9);
+        world[4][10] = new Sprite(Assets.WALL, 4, 10);
         //Columna X=5
-        world[5][4] = new Sprite(Assets.WALL, 3, 9);
-        world[5][6] = new Sprite(Assets.BOX, 3, 9);
-        world[5][7] = new Sprite(Assets.Player, 3, 9);
-        world[5][8] = new Sprite(Assets.FLOOR, false, 3, 9);
-        world[5][9] = new Sprite(Assets.FLOOR, false, 3, 9);
-        world[5][10] = new Sprite(Assets.WALL, 3, 9);
+        world[5][4] = new Sprite(Assets.WALL, 5, 4);
+        world[5][6] = new Sprite(Assets.BOX, 5, 6);
+        world[5][7] = new Sprite(Assets.PLAYER, 5, 7);
+        world[5][8] = new Sprite(Assets.FLOOR, false, 5, 8);
+        world[5][9] = new Sprite(Assets.FLOOR, false, 5, 9);
+        world[5][10] = new Sprite(Assets.WALL, 5, 10);
         //Columna X=6
-        world[6][4] = new Sprite(Assets.WALL, 3, 9);
-        world[6][6] = new Sprite(Assets.WALL, 3, 9);
-        world[6][8] = new Sprite(Assets.FLOOR, false, 3, 9);
-        world[6][9] = new Sprite(Assets.FLOOR, false, 3, 9);
-        world[6][10] = new Sprite(Assets.WALL, 3, 9);
+        world[6][4] = new Sprite(Assets.WALL, 6, 4);
+        world[6][6] = new Sprite(Assets.WALL, 6, 6);
+        world[6][8] = new Sprite(Assets.FLOOR, false, 6, 8);
+        world[6][9] = new Sprite(Assets.FLOOR, false, 6, 9);
+        world[6][10] = new Sprite(Assets.WALL, 6, 10);
         //Columna X=7
-        world[7][4] = new Sprite(Assets.WALL, 3, 9);
-        world[7][6] = new Sprite(Assets.WALL, 3, 9);
-        world[7][7] = new Sprite(Assets.BOX, 3, 9);
-        world[7][8] = new Sprite(Assets.WALL, 3, 9);
-        world[7][9] = new Sprite(Assets.WALL, 3, 9);
-        world[7][10] = new Sprite(Assets.WALL, 3, 9);
+        world[7][4] = new Sprite(Assets.WALL, 7, 4);
+        world[7][6] = new Sprite(Assets.WALL, 7, 6);
+        world[7][7] = new Sprite(Assets.BOX, 7, 7);
+        world[7][8] = new Sprite(Assets.WALL, 7, 8);
+        world[7][9] = new Sprite(Assets.WALL, 7, 9);
+        world[7][10] = new Sprite(Assets.WALL, 7, 10);
         //Columna X=8
-        world[8][4] = new Sprite(Assets.WALL, 3, 9);
-        world[8][6] = new Sprite(Assets.WALL, 3, 9);
-        world[8][10] = new Sprite(Assets.WALL, 3, 9);
+        world[8][4] = new Sprite(Assets.WALL, 8, 4);
+        world[8][6] = new Sprite(Assets.WALL, 8, 6);
+        world[8][10] = new Sprite(Assets.WALL, 8, 10);
         //Columna X=9
-        world[9][4] = new Sprite(Assets.WALL, 3, 9);
-        world[9][8] = new Sprite(Assets.BOX, 3, 9);
-        world[9][10] = new Sprite(Assets.WALL, 3, 9);
+        world[9][4] = new Sprite(Assets.WALL, 9, 4);
+        world[9][8] = new Sprite(Assets.BOX, 9, 8);
+        world[9][10] = new Sprite(Assets.WALL, 9, 10);
         //Columna X=10
-        world[10][4] = new Sprite(Assets.WALL, 3, 9);
-        world[10][5] = new Sprite(Assets.WALL, 3, 9);
-        world[10][7] = new Sprite(Assets.BOX, 3, 9);
-        world[10][10] = new Sprite(Assets.WALL, 3, 9);
+        world[10][4] = new Sprite(Assets.WALL, 10, 4);
+        world[10][5] = new Sprite(Assets.WALL, 10, 5);
+        world[10][7] = new Sprite(Assets.BOX, 10, 7);
+        world[10][10] = new Sprite(Assets.WALL, 10, 10);
         //Columna X=11
-        world[11][5] = new Sprite(Assets.WALL, 3, 9);
-        world[11][8] = new Sprite(Assets.WALL, 3, 9);
-        world[11][9] = new Sprite(Assets.WALL, 3, 9);
-        world[11][10] = new Sprite(Assets.WALL, 3, 9);
+        world[11][5] = new Sprite(Assets.WALL, 11, 5);
+        world[11][8] = new Sprite(Assets.WALL, 11, 8);
+        world[11][9] = new Sprite(Assets.WALL, 11, 9);
+        world[11][10] = new Sprite(Assets.WALL, 11, 10);
         //Columna X=12
-        world[12][5] = new Sprite(Assets.WALL, 3, 9);
-        world[12][6] = new Sprite(Assets.WALL, 3, 9);
-        world[12][7] = new Sprite(Assets.WALL, 3, 9);
-        world[12][8] = new Sprite(Assets.WALL, 3, 9);
+        world[12][5] = new Sprite(Assets.WALL, 12, 5);
+        world[12][6] = new Sprite(Assets.WALL, 12, 6);
+        world[12][7] = new Sprite(Assets.WALL, 12, 7);
+        world[12][8] = new Sprite(Assets.WALL, 12, 8);
     }
-    
+    /*
     @Override
     public void paint(Graphics g){
         super.paint(g);
@@ -114,7 +109,7 @@ public class Canvas3 extends JPanel implements Runnable{
             }
         }
         g.drawImage(bufferedImage, 0, 0, this);
-    }
+    }*/
     
     @Override
     public void run() {
